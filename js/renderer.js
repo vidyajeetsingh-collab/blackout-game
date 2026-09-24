76eb9892851fa76ffbce07a4fc48f318a2fd3a19
vidyajeetsingh-collab <vidyajeetsingh@gmail.com>
2026-09-24 17:55:23 +0530

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,52 +1,27 @@
-// PROJECT: BLACKOUT
-// WebGL Diagnostic Renderer
-
 const Renderer = {
-
-    gl: null,
-
     init() {
-
-        this.gl = Engine.gl;
-
-        console.log("RENDERER TEST INIT");
-
-        if (!this.gl) {
-            console.error("NO WEBGL CONTEXT");
-            return;
-        }
-
-        console.log(
-            "WEBGL:",
-            this.gl.getParameter(
-                this.gl.VERSION
-            )
-        );
+        console.log("RENDERER INIT");
     },
 
     render() {
+        const canvas = document.getElementById("gameCanvas");
 
-        if (!this.gl) {
+        if (!canvas) {
+            alert("CANVAS NOT FOUND");
             return;
         }
 
-        this.gl.viewport(
-            0,
-            0,
-            this.gl.drawingBufferWidth,
-            this.gl.drawingBufferHeight
-        );
+        canvas.style.background = "red";
+        canvas.style.display = "block";
+        canvas.style.width = "100vw";
+        canvas.style.height = "100vh";
 
-        this.gl.clearColor(
-            0.8,
-            0.05,
-            0.05,
-            1.0
-        );
-
-        this.gl.clear(
-            this.gl.COLOR_BUFFER_BIT |
-            this.gl.DEPTH_BUFFER_BIT
+        console.log(
+            "CANVAS:",
+            canvas.width,
+            canvas.height,
+            canvas.clientWidth,
+            canvas.clientHeight
         );
     }
 };
