51723d9ba3840d91a57135da776b9218fcdaa4c2
vidyajeetsingh-collab <vidyajeetsingh@gmail.com>
2026-09-25 04:53:51 +0530

    Update world.js

--- js/world.js
+++ js/world.js
@@ -1,140 +1,40 @@
 // PROJECT: BLACKOUT
-// World + City Area System
+// World System
 
 const World = {
 
-    areas: {
-        RESIDENTIAL: {
-            name: "RESIDENTIAL",
-            unlocked: true
-        },
-
-        DOWNTOWN: {
-            name: "DOWNTOWN",
-            unlocked: false
-        },
-
-        COMMERCIAL: {
-            name: "COMMERCIAL",
-            unlocked: false
-        },
-
-        INDUSTRIAL: {
-            name: "INDUSTRIAL",
-            unlocked: false
-        },
-
-        UNDERGROUND: {
-            name: "UNDERGROUND",
-            unlocked: false
-        }
-    },
-
-    currentArea: "RESIDENTIAL",
+    initialized: false,
 
     init() {
 
-        this.currentArea = "RESIDENTIAL";
-
-        this.areas.RESIDENTIAL.unlocked = true;
+        this.initialized = true;
 
         console.log(
-            "World initialized."
+            "BLACKOUT World initialized."
         );
     },
 
-    updateArea(areaName) {
+    update(deltaTime) {
 
-        if (!this.areas[areaName]) {
-            return false;
-        }
+        // World update loop.
+        // Kept lightweight for mobile performance.
 
-        if (!this.areas[areaName].unlocked) {
-            console.log(
-                "Area locked:",
-                areaName
-            );
-
-            return false;
-        }
-
-        this.currentArea = areaName;
-
-        console.log(
-            "Current area:",
-            areaName
-        );
-
-        return true;
-    },
-
-    unlockArea(areaName) {
-
-        if (!this.areas[areaName]) {
+        if (!this.initialized) {
             return;
         }
 
-        this.areas[areaName].unlocked = true;
-
-        console.log(
-            "Area unlocked:",
-            areaName
-        );
-    },
-
-    unlockMissionArea(missionNumber) {
-
-        const order = [
-            "RESIDENTIAL",
-            "DOWNTOWN",
-            "COMMERCIAL",
-            "INDUSTRIAL",
-            "UNDERGROUND"
-        ];
-
-        const index =
-            missionNumber - 1;
-
-        if (
-            index >= 0 &&
-            index < order.length
-        ) {
-            this.unlockArea(
-                order[index]
-            );
-        }
-    },
-
-    isUnlocked(areaName) {
-
-        return !!(
-            this.areas[areaName] &&
-            this.areas[areaName].unlocked
-        );
-    },
-
-    getCurrentArea() {
-
-        return this.currentArea;
-    },
-
-    getUnlockedAreas() {
-
-        return Object.values(
-            this.areas
-        ).filter(
-            area => area.unlocked
-        );
+        // Future systems can be updated here:
+        // buildings
+        // traffic
+        // lighting
+        // weather
+        // mission areas
+        // destructible objects
     },
 
     reset() {
 
-        for (const key in this.areas) {
-            this.areas[key].unlocked =
-                key === "RESIDENTIAL";
-        }
+        this.initialized = false;
 
-        this.currentArea =
-            "RESIDENTIAL";
     }
 };
